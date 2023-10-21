63405c25827e9cdf33efbd56ad94dd3c58e19f31
Ivan Kratchanov <ivankra@gmail.com>
2023-10-21 22:58:32 +0300

    Add files via upload

---
+++ 06.CardsGame/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> firstHand = Console.ReadLine().Split().Select(int.Parse).ToList();
+        List<int> secondHand = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        while (firstHand.Count > 0 && secondHand.Count > 0)
+        {
+            int firstPlayerCard = firstHand[0];
+            int secondPlayerCard = secondHand[0];
+
+            if (firstPlayerCard > secondPlayerCard)
+            {
+                firstHand.RemoveAt(0);
+                secondHand.RemoveAt(0);
+                firstHand.Add(secondPlayerCard);
+                firstHand.Add(firstPlayerCard);
+            }
+            else if (secondPlayerCard > firstPlayerCard)
+            {
+                firstHand.RemoveAt(0);
+                secondHand.RemoveAt(0);
+                secondHand.Add(firstPlayerCard);
+                secondHand.Add(secondPlayerCard);
+            }
+            else
+            {
+                firstHand.RemoveAt(0);
+                secondHand.RemoveAt(0);
+            }
+        }
+
+        if (firstHand.Count > 0)
+        {
+            int sum = firstHand.Sum();
+            Console.WriteLine($"First player wins! Sum: {sum}");
+        }
+        else
+        {
+            int sum = secondHand.Sum();
+            Console.WriteLine($"Second player wins! Sum: {sum}");
+        }
+    }
+}
---
+++ 07.AppendArrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputArrays = Console.ReadLine().Split('|').Reverse().ToArray();
+        List<int> result = new List<int>();
+
+        foreach (string array in inputArrays)
+        {
+            int[] numbers = array.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            result.AddRange(numbers);
+        }
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
